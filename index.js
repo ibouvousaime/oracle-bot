b8257fe4e0147dd069fbd54c788cd75fb3a248c9
Abdoulaye Ibrahima Sarr <ibouvousaime@gmail.com>
2022-09-14 16:20:09 +0000

    added links

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Tgfancy = require("tgfancy");
 const oracleData = require('./oracle-data.json')
 
 const bot = new Tgfancy(process.env.telegram_token, {
-    polling:true,
+    polling:true, 
     // all options to 'tgfancy' MUST be placed under the
     // 'tgfancy' key, as shown below
     tgfancy: {
@@ -12,22 +12,25 @@ const bot = new Tgfancy(process.env.telegram_token, {
 });
 
 function getDiceNumbers(){
-    return [Math.floor(Math.random() * 5) + 1,Math.floor(Math.random() * 5) + 1,Math.floor(Math.random() * 5) + 1]
+    return [Math.floor(Math.random() * 6) + 1,Math.floor(Math.random() * 6) + 1,Math.floor(Math.random() * 6) + 1]
 }
 
 function getRandomOracleMessage() {
     let numbers = getDiceNumbers()
-    console.log(numbers)
     let message = oracleData.filter(line=> line["#1"] == numbers[0] && line["#2"] == numbers[1] && line["#3"] == numbers[2])[0]
-    return message
+    return message.Oracle.includes("—") ? getRandomOracleMessage() : message
+}
+
+function generateGoogleLuckyLink(source, book, line) {
+    return `http://www.google.com/search?q=${source}+${book}+${line}&btnI`
 }
 
 bot.on('message', (msg)=> {
     const chatId = msg.chat.id;
     let message = getRandomOracleMessage()
-    let formattedOracleMessage = `${message.Oracle} (Source: ${message.Source}, Book: ${message.Book}, Line : ${message.Line})`
-    bot.sendMessage(chatId,formattedOracleMessage).then(()=> {
-
+    let formattedOracleMessage = `${message.Oracle} [Source: ${message.Source}, Book: ${message.Book}, Line : ${message.Line}](${generateGoogleLuckyLink(message.Source, message.Book, message.Line)})`
+    bot.sendMessage(chatId,formattedOracleMessage, {parse_mode:"MarkdownV2"}).then(()=> {
+        console.log("sent", message)
     }).catch(err=> {
         console.error(err)
     })
