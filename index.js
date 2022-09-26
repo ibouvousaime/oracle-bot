5915a27f44ea3f0b2185658777e0441c5a721127
Abdoulaye Ibrahima Sarr <ibouvousaime@gmail.com>
2022-09-26 12:48:16 +0000

    reorganising

--- index.js
+++ index.js
@@ -6,30 +6,58 @@ const bot = new Tgfancy(process.env.telegram_token, {
     polling:true, 
     tgfancy: {
         option: "value",
-    },
+    }
 });
 
+
 function getDiceNumbers(){
-    return [Math.floor(Math.random() * 6) + 1,Math.floor(Math.random() * 6) + 1,Math.floor(Math.random() * 6) + 1]
+    return [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1]
 }
+
 
 function getRandomOracleMessage() {
     let numbers = getDiceNumbers()
-    let message = oracleData.filter(line=> line["#1"] == numbers[0] && line["#2"] == numbers[1] && line["#3"] == numbers[2])[0]
+    let message = oracleData.filter(line => line["#1"] == numbers[0] && line["#2"] == numbers[1] && line["#3"] == numbers[2])[0]
     return message.Oracle.includes("—") ? getRandomOracleMessage() : message
 }
+
+
 
 function generateGoogleLuckyLink(source, book, line) {
     return `http://www.google.com/search?q=${source}+${book}+${line}&btnI`
 }
 
-bot.on('message', (msg)=> {
-    const chatId = msg.chat.id;
-    let message = getRandomOracleMessage()
-    let formattedOracleMessage = `${message.Oracle} [Source: ${message.Source}, Book: ${message.Book}, Line : ${message.Line}](${generateGoogleLuckyLink(message.Source, message.Book, message.Line)})`
-    bot.sendMessage(chatId,formattedOracleMessage, {parse_mode:"MarkdownV2"}).then(()=> {
-        console.log("sent", message)
-    }).catch(err=> {
-        console.error(err)
+
+function sendOracleMessage(chatId, msg) {
+    return new Promise((resolve, reject)=> {
+        let message = getRandomOracleMessage()
+        const sourceLink = `${generateGoogleLuckyLink(message.Source, message.Book, message.Line)}`
+        let formattedOracleMessage = `The oracle's message to ${msg.text}: <i>${message.Oracle}</i> <a href="${sourceLink}"> Source: ${message.Source}, Book: ${message.Book}, Line : ${message.Line} </a>`
+        bot.sendMessage(chatId, formattedOracleMessage, {parse_mode:"HTML", disable_web_page_preview: true}).then(()=> {
+            console.log("sent", formattedOracleMessage)
+            resolve()
+        }).catch(err=> {
+            console.error(err)
+            reject()
+        })
     })
+
+}
+
+bot.on('message', async (msg)=> {
+    const chatId = msg.chat.id;
+    if (msg.text.trim().length && !msg.text.includes("/")) {
+        sendOracleMessage().catch(err=> {
+            sendOracleMessage(chatId, msg)
+        })
+    }
+    else if (msg.text == '/start') {
+        bot.sendMessage(chatId, "Welcome, please send me someone's name and I will predict their future.")
+    }
+    else {
+        bot.sendMessage(chatId, "Send me a name instead, please.").catch(err=> {
+            console.error(err)
+        })
+    }
 })
+
