1292668f8fc1c966990f46152b093cdd2e279b68
Abdoulaye Ibrahima Sarr <ibouvousaime@gmail.com>
2022-10-07 00:46:59 +0000

    new format

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function sendOracleMessage(chatId, msg) {
     return new Promise((resolve, reject)=> {
         let message = getRandomOracleMessage()
         const sourceLink = `${generateGoogleLuckyLink(message.Source, message.Book, message.Line)}`
-        let formattedOracleMessage = `The oracle's message to ${msg.text}: <i>${message.Oracle}</i> <a href="${sourceLink}"> Source: ${message.Source}, Book: ${message.Book}, Line : ${message.Line} </a>`
+        let formattedOracleMessage = `The oracle's message to ${msg.text}: <i>${message.Oracle}</i> <a href="${sourceLink}"> Source: ${message.Source}, Book: ${message.Book}, Line : ${message.Line}</a> <code>(${message.Source}  ${message.Book} ${message.Line})</code>`
         bot.sendMessage(chatId, formattedOracleMessage, {parse_mode:"HTML", disable_web_page_preview: true}).then(()=> {
             console.log("sent", formattedOracleMessage)
             resolve()
